a0f9e31e8c61d44b4267e5fa1050829da0f11918
Xian, Tienan <tienan.xian@tekcomms.com>
2014-12-12 15:32:07 +0800

    Commit for FindPeakElement

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 LeetCode
 ========
+
+https://oj.leetcode.com/problems/
---
+++ src/findpeakelement/FindPeakElement.java
@@ -0,0 +1,42 @@
+//A peak element is an element that is greater than its neighbors.
+//
+//Given an input array where num[i] ¡Ù num[i+1], find a peak element and return its index.
+//
+//The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+//
+//You may imagine that num[-1] = num[n] = -¡Þ.
+//
+//For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+//Your solution should be in logarithmic complexity.
+
+package findpeakelement;
+
+public class FindPeakElement {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.print(findPeakElement(new int[] { 1, 2, 3, 1 }));
+
+    }
+
+    public static int findPeakElement(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            if (i == 0 && num[i] > num[i + 1]) {
+                return i;
+            }
+
+            if (i == num.length - 1 && num[i] > num[i - 1]) {
+                return i;
+            }
+
+            if (num[i] > num[i + 1] && num[i] > num[i - 1]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
